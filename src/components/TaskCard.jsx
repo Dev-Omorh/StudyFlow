2fc3fd5a635d7cd8ce366deb113225c4e242f7ee
omoladeajala <shadowcoderx.02@gmail.com>
2026-07-11 12:37:34 +0100

    Refactor TaskCard and Tasks components; update TaskCard to accept task props and render task details, and modify Tasks to manage task state and render TaskCard instances dynamically

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -1,7 +1,14 @@
-function TaskCard() {
+function TaskCard({ task }) {
   return (
-    <div>
-      <input type="checkbox" />
+    <div className="task-card">
+      <input type="checkbox" checkbox={task.completed} />
+
+      <div>
+        <h3>{task.title}</h3>
+        <p>{task.course}</p>
+      </div>
+
+      <span>{task.prioriy}</span>
     </div>
   );
 }
--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -5,7 +5,24 @@ import Sidebar from "../components/Sidebar";
 import TaskCard from "../components/TaskCard";
 import TaskModal from "../components/TaskModal";
 
-const [tasks, setTasks] = useState("");
+const [tasks, setTasks] = useState([
+  {
+    id: 1,
+    title: "Complete Problem Set 5",
+    course: "MATH 202",
+    priority: "High",
+    completed: false,
+    dueDate: "2026-07-15"
+  },
+  {
+    id: 2,
+    title: "Read Chapter 7",
+    course: "PSY 101",
+    priority: "Medium",
+    completed: false,
+    dueDate: "2026-07-16"
+  },
+]);
 
 function deleteTask(id) {
   setTasks(tasks.filter((task) => task.id !== id));
@@ -22,7 +39,7 @@ function addTask(newTask) {
 }
 
 function editTask(id, updatedTask) {
-  setTasks([...tasks.editTasks]);
+  ...
 }
 
 function Tasks() {
@@ -33,10 +50,14 @@ function Tasks() {
 
       <FilterButton />
 
-      <TaskCard />
-      <TaskCard />
-      <TaskCard />
-      <TaskCard />
+      {task.map((task) => 
+      <TaskCard
+      key={task.id} 
+      task={task} 
+      onDelete={deletetask} 
+      onEdit={editTask}
+      onToggle={toggleComplete}  />)}
+      
 
       <TaskModal />
     </>
