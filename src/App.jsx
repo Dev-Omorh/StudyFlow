ae78e8d6cdd3f87ee8cea2ddbcb4ce1f3286e5d3
omoladeajala <shadowcoderx.02@gmail.com>
2026-07-13 11:39:35 +0100

    Implement routing for main application pages; replace Dashboard with BrowserRouter and Routes in App component, and update Sidebar for navigation links

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
+import { BrowerRouter, Routes, Route } from "react-router-dom";
+import Assignment from "./pages/Assignments";
+import Examcountdown from "./pages/ExamCountdown";
 import Dashboard from "./pages/Dashboard";
+import Tasks from "./pages/Tasks";
+import Courses from "./pages/Courses";
+import Notes from "./pages/Note";
 
 function App() {
   return (
     <>
-      <Dashboard />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/notes" element={<Dashboard />} />
+          <Route path="/tasks" element={<Tasks />} />
+          <Route path="/courses" element={<Courses />} />
+          <Route path="/assignment" element={<Assignment />} />
+          <Route path="/examcountdown" element={<Examcountdown />} />
+          <Route path="/notes" element={<Notes />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,19 +1,21 @@
+import { NavLink } from "react-route-dom";
+import Assignment from "../pages/Assignments";
+import Examcountdown from "../pages/ExamCountdown";
 import Dashboard from "../pages/Dashboard";
 import Tasks from "../pages/Tasks";
 import Courses from "../pages/Courses";
-import Assignment from "../pages/Assignments";
-import Examcountdown from "../pages/Exam countdown";
 import Notes from "../pages/Note";
 
 function SideBar() {
   return (
     <div>
-      <Dashboard />
-      <Tasks />
-      <Courses />
-      <Assignment />
-      <Examcountdown />
-      <Notes />
+      <NavLink to="/">Dashboard </NavLink>
+      <NavLink to="/tasks">Tasks </NavLink>
+      <NavLink to="/courses">Courses</NavLink>
+      <NavLink to="/assignment">Assignment </NavLink>
+      <NavLink to="/examcountdown">Examcountdown </NavLink>
+
+      <NavLink to="/notes">Notes</NavLink>
     </div>
   );
 }
